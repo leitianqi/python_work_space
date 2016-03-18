c81ee72b28623082b63d9a71b1bd19ba0f01345a
leitianqi800 <670748657@qq.com>
2016-03-18 14:49:30 +0800

    update somefiles

--- hanoi.py
+++ hanoi.py
@@ -1,9 +1,15 @@
+# -*- coding:utf-8 -*-
+
 def hanoi(n,a,b,c):
 	if n==1:
-		print('move',a ,'to',c)
+		print('move',a,'to',c)
 		return
 	else:
 		hanoi(n-1,a,c,b)
-		print('move',a ,'to',c)
+		print('move',a,'to',c)
 		hanoi(n-1,b,a,c)
-hanoi(4,'a','b','c')
+		
+
+s=input('please input n:')
+n=int(s)
+hanoi(n,'a','b','c')
--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
-name=input()
-print("Hello , World!",name)
-print('nihao') 
+#!/usr/bin/env python3
+#-*- conding:utf-8 -*-
+
+'a test module'
+
+__author__='Tianqi Lei'
+
+import sys
+
+def test():
+	args = sys.argv
+	if len(args)==1:
+		print('Hello,World!')
+	elif len(args)==2:
+		print('Hello,%s!' % args[1])
+	else:
+		print('Too many argument!')
+
+if __name__ == '__main__':
+	test()
